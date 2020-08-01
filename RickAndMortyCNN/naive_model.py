4a6ffb3a33c08b8520d536b1a85f282a1a29cd12
zura <dzindzibadzezurabi@gmail.com>
2020-08-01 15:41:04 +0400

    refactored 2 spaces into 4

--- RickAndMortyCNN/naive_model.py
+++ RickAndMortyCNN/naive_model.py
@@ -1,12 +1,14 @@
+import tensorflow as tf
+
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow import optimizers
-import tensorflow as tf
 
 from data_batching import train_generator
 from data_batching import valid_generator
 
 
+# Simple Stack of Conv2D's and Maxpooling
 conv_model = models.Sequential([
     layers.Conv2D(16, (3, 3), activation='relu', input_shape=(256, 256, 3)),
     layers.Conv2D(32, (3, 3), activation='relu'),
@@ -31,15 +33,15 @@ conv_model = models.Sequential([
 ])
 
 conv_model.compile(
-  loss='binary_crossentropy',
-  optimizer=optimizers.RMSprop(lr=1e-4),
-  metrics=['acc'])
+    loss='binary_crossentropy',
+    optimizer=optimizers.RMSprop(lr=1e-4),
+    metrics=['acc'])
 
 with tf.device('/device:GPU:0'):
-  history = conv_model.fit(
-    train_generator, steps_per_epoch=11,
-    validation_data=valid_generator,
-    validation_steps=6,
-    epochs=30)
+    history = conv_model.fit(
+        train_generator, steps_per_epoch=11,
+        validation_data=valid_generator,
+        validation_steps=6,
+        epochs=80)
 
 conv_model.save('model.h5')
